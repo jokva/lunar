933af6f80293d467ad8e8389410d730e930e41e5
Jørgen Kvalsvik <jokva@statoil.com>
2017-11-11 10:43:45 +0100

    Move tests to Catch2 - abandon boost-test
    Implement the tests in catch2 rather than boost-test, a nice simple take
    on writing tests for C++. Catch itself is header-only and distributed as
    such, so a copy of the current release resides in external/

--- lib/tests/basic-rules.cpp
+++ lib/tests/basic-rules.cpp
@@ -1,279 +1,222 @@
-#define BOOST_TEST_MODULE runspec
-
+#include <algorithm>
+#include <stdexcept>
 #include <string>
-
-#include <boost/test/unit_test.hpp>
 
 #include <lunar/parser.hpp>
 
-namespace lun {
+#include <catch/catch.hpp>
 
-bool operator==( const std::string& key, const keyword& kw ) {
-    return key == kw.name;
+namespace Catch {
+
+template<>
+struct StringMaker< lun::keyword > {
+    static std::string convert( const lun::keyword& kw ) {
+        return kw.name;
+    }
+};
+
 }
 
-bool operator==( const keyword& kw, const std::string& key ) {
-        return key == kw;
+namespace {
+
+class HasKeyword : public Catch::MatcherBase< std::vector< lun::keyword > > {
+    public:
+        HasKeyword( std::string str ) : name( std::move( str ) ) {}
+
+        bool match( const std::vector< lun::keyword >& kws ) const override {
+            for( const auto& kw : kws )
+                if( kw.name == this->name ) return true;
+
+            return false;
+        }
+
+        virtual std::string describe() const override {
+            return "contains " + this->name;
+        }
+
+    private:
+        std::string name;
+};
+
+class IsType : public Catch::MatcherBase< lun::item > {
+    public:
+        IsType( lun::item::tag x ) : type( x ) {}
+
+        bool match( const lun::item& x ) const override {
+            return this->type == x.type;
+        }
+
+        virtual std::string describe() const override {
+            std::stringstream ss;
+            ss << "is of type " << this->type;
+            return ss.str();
+        }
+
+    private:
+        lun::item::tag type;
+};
+
+IsType IsInt() { return IsType( lun::item::tag::i ); }
+IsType IsFloat() { return IsType( lun::item::tag::f ); }
+IsType IsString() { return IsType( lun::item::tag::str ); }
+
+class Repeats : public Catch::MatcherBase< lun::item > {
+    public:
+        Repeats( int n ) : repeats( n ) {}
+
+        bool match( const lun::item& x ) const override {
+            return x.repeat == this->repeats;
+        }
+
+        virtual std::string describe() const override {
+            return "repeats " + std::to_string( this->repeats ) + " times";
+        }
+
+    private:
+        int repeats;
+};
+
+const lun::keyword& get( const std::string& key,
+                         const std::vector< lun::keyword >& kws ) {
+    auto itr = std::find_if( kws.begin(), kws.end(),
+                    [&]( const lun::keyword& x ) { return key == x.name; }
+             );
+
+    if( itr == kws.end() ) throw std::out_of_range( "Key not found: " + key );
+
+    return *itr;
 }
 
-bool operator==( const item& x, int i ) {
-    return x.type == item::tag::i && x.ival == i;
 }
 
-bool operator==( const item& x, double f ) {
-    return x.type == item::tag::f && x.fval == f;
-}
-
-bool operator==( const item& x, const std::string& str ) {
-    return x.type == item::tag::str && x.sval == str;
-}
-
-template< typename T >
-bool operator==( const T& lhs, const item& rhs ) { return rhs == lhs; }
-template< typename T >
-bool operator!=( const T& lhs, const item& rhs ) { return !(rhs == lhs); }
-template< typename T >
-bool operator!=( const item& lhs, const T& rhs ) { return !(lhs == rhs); }
-
-const std::vector< record >& at( const section& sec, const std::string& key ) {
-    auto itr = std::find( sec.xs.begin(), sec.xs.end(), key );
-    if( itr == sec.xs.end() ) throw std::out_of_range("No such key: " + key );
-
-    return itr->xs;
-}
-
-bool has_keyword( const std::vector< keyword >& xs, const std::string& key ) {
-    return std::find( xs.begin(), xs.end(), key ) != xs.end();
-}
-
-BOOST_AUTO_TEST_CASE( text_after_slash ) {
+TEST_CASE( "slash terminates record", "[slash]") {
     const std::string input = R"(
 RUNSPEC
 
 DIMENS
     10 20 30 / text-after-slash
 
-OIL
-)";
+GAS
 
-    auto sec = parse( input.begin(), input.end() ).front();
-    BOOST_CHECK_EQUAL( "RUNSPEC", sec.name );
-    BOOST_CHECK( has_keyword( sec.xs, "DIMENS" ) );
-    BOOST_CHECK( has_keyword( sec.xs, "OIL" ) );
-    auto& dimens = at( sec, "DIMENS" ).at( 0 );
+EQLDIMS
+    2/
 
-    for( const auto& item : dimens )
-        BOOST_CHECK_EQUAL( item.type, item::tag::i );
-}
-
-BOOST_AUTO_TEST_CASE( no_space_before_slash ) {
-    const std::string input = R"(
-RUNSPEC
-
-DIMENS
-    10/
-OIL
-)";
-
-    auto sec = parse( input.begin(), input.end() ).front();
-    BOOST_CHECK_EQUAL( "RUNSPEC", sec.name );
-    BOOST_CHECK( has_keyword( sec.xs, "DIMENS" ) );
-    BOOST_CHECK( has_keyword( sec.xs, "OIL" ) );
-    auto& dimens = at( sec, "DIMENS" ).at( 0 );
-
-    for( const auto& item : dimens )
-        BOOST_CHECK_EQUAL( item.type, item::tag::i );
-}
-
-BOOST_AUTO_TEST_CASE( slash_different_line ) {
-    const std::string input = R"(
-RUNSPEC
-
-DIMENS
+REGDIMS
     10
 /
+
 OIL
 )";
 
-    auto sec = parse( input.begin(), input.end() ).front();
-    BOOST_CHECK_EQUAL( "RUNSPEC", sec.name );
-    BOOST_CHECK( has_keyword( sec.xs, "DIMENS" ) );
-    BOOST_CHECK( has_keyword( sec.xs, "OIL" ) );
-    auto& dimens = at( sec, "DIMENS" ).at( 0 );
+    auto sec = lun::parse( input.begin(), input.end() ).front();
+    REQUIRE( sec.name == "RUNSPEC" );
 
-    for( const auto& item : dimens )
-        BOOST_CHECK_EQUAL( item.type, item::tag::i );
+    SECTION( "toggles have no items" ) {
+        CHECK_THAT( sec.xs, HasKeyword( "OIL" ) );
+        const auto& oil = get( "OIL", sec.xs );
+        CHECK( oil.xs.empty() );
+    }
+
+    SECTION( "integers are recognised" ) {
+        REQUIRE_THAT( sec.xs, HasKeyword( "DIMENS" ) );
+        const auto& kw = get( "DIMENS", sec.xs );
+
+        for( const auto& item : kw.xs ) {
+            REQUIRE( !item.empty() );
+            CHECK_THAT( item[ 0 ], IsInt() );
+        }
+    }
+
+    SECTION( "/ following int does not change the value" ) {
+        REQUIRE_THAT( sec.xs, HasKeyword( "EQLDIMS" ) );
+        const auto& eqldims = get( "EQLDIMS", sec.xs );
+        REQUIRE( !eqldims.xs.empty() );
+        REQUIRE( eqldims.xs[ 0 ].size() == 1 );
+
+        const auto& rec = eqldims.xs[ 0 ];
+        REQUIRE( !rec.empty() );
+        REQUIRE_THAT( rec[ 0 ], IsInt() );
+        CHECK( rec[ 0 ].ival == 2 );
+    }
+
+    SECTION( "/ on new line does not change the value" ) {
+        REQUIRE_THAT( sec.xs, HasKeyword( "REGDIMS" ) );
+        const auto& kw = get( "REGDIMS", sec.xs );
+        REQUIRE( !kw.xs.empty() );
+        REQUIRE( kw.xs[ 0 ].size() == 1 );
+
+        auto& rec = kw.xs[ 0 ];
+        REQUIRE( !rec.empty() );
+        REQUIRE_THAT( rec[ 0 ], IsInt() );
+        CHECK( rec[ 0 ].ival == 10 );
+    }
 }
 
-BOOST_AUTO_TEST_CASE( toggles ) {
-    const std::string input = R"(
-RUNSPEC
-
-WATER
-OIL
-)";
-    auto sec = parse( input.begin(), input.end() ).front();
-    BOOST_CHECK_EQUAL( "RUNSPEC", sec.name );
-    BOOST_CHECK( has_keyword( sec.xs, "WATER" ) );
-    BOOST_CHECK( has_keyword( sec.xs, "OIL" ) );
-}
-
-BOOST_AUTO_TEST_CASE( record_all_int ) {
-    const std::string input = R"(
-RUNSPEC
-
-OIL -- toggle keyword to make the deck less trivial
-
-DIMENS
-    10 20 30 /
-
-WATER
-)";
-    auto sec = parse( input.begin(), input.end() ).front();
-    const auto& dimens = at( sec, "DIMENS" ).at( 0 );
-    int exp[] = { 10, 20, 30 };
-
-    BOOST_CHECK_EQUAL_COLLECTIONS( std::begin( dimens ), std::end( dimens ),
-                                   std::begin( exp ), std::end( exp ) );
-}
-
-BOOST_AUTO_TEST_CASE( repeat_int ) {
+TEST_CASE( "ints with repetition", "[repeat]") {
     const std::string input = R"(
 RUNSPEC
 
 EQLDIMS
     3*5 /
-)";
-    auto sec = parse( input.begin(), input.end() ).front();
-    const auto& kw = at( sec, "EQLDIMS" ).at( 0 );
-
-    BOOST_CHECK_EQUAL( 1U, kw.size() );
-
-    const auto& x = kw.at( 0 );
-    BOOST_CHECK_EQUAL( 3, x.repeat );
-    BOOST_CHECK_EQUAL( item::tag::i, x.type );
-    BOOST_CHECK_EQUAL( 5, x.ival );
-}
-
-BOOST_AUTO_TEST_CASE( repeat_int_mixed ) {
-    const std::string input = R"(
-RUNSPEC
 
 DIMENS
     5 2*10 /
+
 )";
 
-    auto sec = parse( input.begin(), input.end() ).front();
-    const auto& kw = at( sec, "DIMENS" ).at( 0 );
-    BOOST_CHECK_EQUAL( 2U, kw.size() );
+    auto sec = lun::parse( input.begin(), input.end() ).front();
+    REQUIRE( sec.name == "RUNSPEC" );
 
-    const auto& x1 = kw.at( 0 );
-    BOOST_CHECK_EQUAL( 1, x1.repeat );
-    BOOST_CHECK_EQUAL( item::tag::i, x1.type );
-    BOOST_CHECK_EQUAL( 5, x1.ival );
+    SECTION( "a single int" ) {
+        REQUIRE_THAT( sec.xs, HasKeyword( "EQLDIMS" ) );
 
-    const auto& x2 = kw.at( 1 );
-    BOOST_CHECK_EQUAL( 2, x2.repeat );
-    BOOST_CHECK_EQUAL( item::tag::i, x2.type );
-    BOOST_CHECK_EQUAL( 10, x2.ival );
+        const auto& kw = get( "EQLDIMS", sec.xs );
+        REQUIRE( kw.xs.size() == 1 );
+        REQUIRE( kw.xs[ 0 ].size() == 1 );
+
+        const auto& x = kw.xs[ 0 ][ 0 ];
+        CHECK_THAT( x, Repeats( 3 ) );
+        REQUIRE_THAT( x, IsInt() );
+        CHECK( x.ival == 5 );
+    }
+
+    SECTION( "int, repeated int" ) {
+        REQUIRE_THAT( sec.xs, HasKeyword( "DIMENS" ) );
+
+        const auto& kw = get( "DIMENS", sec.xs );
+        REQUIRE( kw.xs.size() == 1 );
+        REQUIRE( kw.xs[ 0 ].size() == 2 );
+
+        SECTION( "the single integer" ) {
+            const auto& x = kw.xs[ 0 ][ 0 ];
+            REQUIRE_THAT( x, IsInt() );
+            CHECK( x.ival == 5 );
+        }
+
+        SECTION( "the repeated integer" ) {
+            const auto& x = kw.xs[ 0 ][ 1 ];
+            REQUIRE_THAT( x, IsInt() );
+            CHECK_THAT( x, Repeats( 2 ) );
+            CHECK( x.ival == 10 );
+        }
+    }
 }
 
-BOOST_AUTO_TEST_CASE( repeat_float ) {
+TEST_CASE( "floating point numbers", "[float]") {
     const std::string input = R"(
 GRID
 
 MAPAXES
     3*100. 2*13.1 4*.3 /
-)";
-    auto sec = parse( input.begin(), input.end() ).front();
-    const auto& kw = at( sec, "MAPAXES" ).at( 0 );
-
-    const auto& x1 = kw.at( 0 );
-    BOOST_CHECK_EQUAL( 3, x1.repeat );
-    BOOST_CHECK_EQUAL( item::tag::f, x1.type );
-    BOOST_CHECK_CLOSE( 100.0, x1.fval, 1e-5 );
-
-    const auto& x2 = kw.at( 1 );
-    BOOST_CHECK_EQUAL( 2, x2.repeat );
-    BOOST_CHECK_EQUAL( item::tag::f, x2.type );
-    BOOST_CHECK_CLOSE( 13.1, x2.fval, 1e-5 );
-
-    const auto& x3 = kw.at( 2 );
-    BOOST_CHECK_EQUAL( 4, x3.repeat );
-    BOOST_CHECK_EQUAL( item::tag::f, x3.type );
-    BOOST_CHECK_CLOSE( .3, x3.fval, 1e-5 );
-}
-
-BOOST_AUTO_TEST_CASE( repeat_float_mixed ) {
-    const std::string input = R"(
-GRID
 
 MAPAXES
-    1.2 2*2.4 .8 /
-)";
-    auto sec = parse( input.begin(), input.end() ).front();
-    const auto& kw = at( sec, "MAPAXES" ).at( 0 );
-
-    const auto& x1 = kw.at( 0 );
-    BOOST_CHECK_EQUAL( 1, x1.repeat );
-    BOOST_CHECK_EQUAL( item::tag::f, x1.type );
-    BOOST_CHECK_CLOSE( 1.2, x1.fval, 1e-5 );
-
-    const auto& x2 = kw.at( 1 );
-    BOOST_CHECK_EQUAL( 2, x2.repeat );
-    BOOST_CHECK_EQUAL( item::tag::f, x2.type );
-    BOOST_CHECK_CLOSE( 2.4, x2.fval, 1e-5 );
-
-    const auto& x3 = kw.at( 2 );
-    BOOST_CHECK_EQUAL( 1, x3.repeat );
-    BOOST_CHECK_EQUAL( item::tag::f, x3.type );
-    BOOST_CHECK_CLOSE( .8, x3.fval, 1e-5 );
-}
-
-BOOST_AUTO_TEST_CASE( float_without_exponent ) {
-    const std::string input = R"(
-GRID
+    1.2 2*2.4 .8 8.0 8. /
 
 MAPAXES
-    .5 0.5 0.500 50. /
-)";
-    auto sec = parse( input.begin(), input.end() ).front();
-    const auto& kw = at( sec, "MAPAXES" ).at( 0 );
-
-    for( int i = 0; i < 3; ++i ) {
-        const auto& x = kw.at( 0 );
-        BOOST_CHECK_EQUAL( 1, x.repeat );
-        BOOST_CHECK_EQUAL( item::tag::f, x.type );
-        BOOST_CHECK_CLOSE( 0.5, x.fval, 1e-5 );
-    }
-
-    const auto& x = kw.at( 3 );
-    BOOST_CHECK_EQUAL( 1, x.repeat );
-    BOOST_CHECK_EQUAL( item::tag::f, x.type );
-    BOOST_CHECK_CLOSE( 50, x.fval, 1e-5 );
-}
-
-BOOST_AUTO_TEST_CASE( negative_float_without_exponent ) {
-    const std::string input = R"(
-GRID
+    .5 0.5 0.500 /
 
 MAPAXES
     -.5 -0.5 -0.500 /
-)";
-    auto sec = parse( input.begin(), input.end() ).front();
-    const auto& kw = at( sec, "MAPAXES" ).at( 0 );
-
-    for( int i = 0; i < 3; ++i ) {
-        const auto& x = kw.at( 0 );
-        BOOST_CHECK_EQUAL( 1, x.repeat );
-        BOOST_CHECK_EQUAL( item::tag::f, x.type );
-        BOOST_CHECK_CLOSE( -0.5, x.fval, 1e-5 );
-    }
-}
-
-BOOST_AUTO_TEST_CASE( float_with_exponent ) {
-    const std::string input = R"(
-GRID
 
 MAPAXES
     .5e2 0.5e2 0.500e2
@@ -281,21 +224,6 @@ MAPAXES
     .5d2 0.5d2 0.500d2
     .5D2 0.5D2 0.500D2
 /
-)";
-    auto sec = parse( input.begin(), input.end() ).front();
-    const auto& kw = at( sec, "MAPAXES" ).at( 0 );
-
-    for( int i = 0; i < 6; ++i ) {
-        const auto& x = kw.at( 0 );
-        BOOST_CHECK_EQUAL( 1, x.repeat );
-        BOOST_CHECK_EQUAL( item::tag::f, x.type );
-        BOOST_CHECK_CLOSE( 0.5e2, x.fval, 1e-5 );
-    }
-}
-
-BOOST_AUTO_TEST_CASE( negative_float_with_exponent ) {
-    const std::string input = R"(
-GRID
 
 MAPAXES
     -.5e2 -0.5e2 -0.500e2
@@ -303,21 +231,6 @@ MAPAXES
     -.5d2 -0.5d2 -0.500d2
     -.5D2 -0.5D2 -0.500D2
 /
-)";
-    auto sec = parse( input.begin(), input.end() ).front();
-    const auto& kw = at( sec, "MAPAXES" ).at( 0 );
-
-    for( int i = 0; i < 6; ++i ) {
-        const auto& x = kw.at( 0 );
-        BOOST_CHECK_EQUAL( 1, x.repeat );
-        BOOST_CHECK_EQUAL( item::tag::f, x.type );
-        BOOST_CHECK_CLOSE( -0.5e2, x.fval, 1e-5 );
-    }
-}
-
-BOOST_AUTO_TEST_CASE( float_with_negative_exponent ) {
-    const std::string input = R"(
-GRID
 
 MAPAXES
     .5e-2 0.5e-2 0.500e-2
@@ -325,67 +238,177 @@ MAPAXES
     .5d-2 0.5d-2 0.500d-2
     .5D-2 0.5D-2 0.500D-2
 /
+
 )";
-    auto sec = parse( input.begin(), input.end() ).front();
-    const auto& kw = at( sec, "MAPAXES" ).at( 0 );
+    auto sec = lun::parse( input.begin(), input.end() ).front();
+    REQUIRE( sec.name == "GRID" );
+    REQUIRE( sec.xs.size() == 7 );
 
-    for( int i = 0; i < 6; ++i ) {
-        const auto& x = kw.at( 0 );
-        BOOST_CHECK_EQUAL( 1, x.repeat );
-        BOOST_CHECK_EQUAL( item::tag::f, x.type );
-        BOOST_CHECK_CLOSE( 0.5e-2, x.fval, 1e-5 );
+    for( const auto& kw : sec.xs )
+        CHECK( kw.name == "MAPAXES" );
+
+    for( const auto& kw : sec.xs )
+        REQUIRE( kw.xs.size() == 1 );
+
+    SECTION( "repeating floats" ) {
+        const auto& kw = sec.xs[ 0 ];
+        const auto& rec = kw.xs.front();
+        REQUIRE( rec.size() == 3 );
+
+        SECTION( "3*100." ) {
+            const auto& item = rec[ 0 ];
+            CHECK_THAT( item, Repeats( 3 ) );
+            CHECK_THAT( item, IsFloat() );
+            CHECK( item.fval == Approx( 100 ) );
+        }
+
+        SECTION( "2*13.1" ) {
+            const auto& item = rec[ 1 ];
+            CHECK_THAT( item, Repeats( 2 ) );
+            CHECK_THAT( item, IsFloat() );
+            CHECK( item.fval == Approx( 13.1 ) );
+        }
+
+        SECTION( "4*.3" ) {
+            const auto& item = rec[ 2 ];
+            CHECK_THAT( item, Repeats( 4 ) );
+            CHECK_THAT( item, IsFloat() );
+            CHECK( item.fval == Approx( .3 ) );
+        }
+    }
+
+    SECTION( "mix repeated and non-repeated" ) {
+        const auto& kw = sec.xs[ 1 ];
+        const auto& rec = kw.xs.front();
+        REQUIRE( rec.size() == 5 );
+
+        SECTION( "1.2" ) {
+            const auto& item = rec[ 0 ];
+            CHECK_THAT( item, IsFloat() );
+            CHECK( item.fval == Approx( 1.2 ) );
+        }
+
+        SECTION( "2*2.4" ) {
+            const auto& item = rec[ 1 ];
+            CHECK_THAT( item, Repeats( 2 ) );
+            CHECK_THAT( item, IsFloat() );
+            CHECK( item.fval == Approx( 2.4 ) );
+        }
+
+        SECTION( ".8" ) {
+            const auto& item = rec[ 2 ];
+            CHECK_THAT( item, IsFloat() );
+            CHECK( item.fval == Approx( 0.8 ) );
+        }
+
+        SECTION( "8.0" ) {
+            const auto& item = rec[ 3 ];
+            CHECK_THAT( item, IsFloat() );
+            CHECK( item.fval == Approx( 8 ) );
+        }
+
+        SECTION( "8." ) {
+            const auto& item = rec[ 4 ];
+            CHECK_THAT( item, IsFloat() );
+            CHECK( item.fval == Approx( 8 ) );
+        }
+    }
+
+    SECTION( "can be written without exponent" ) {
+        for( const auto& x : sec.xs[ 2 ].xs.front() ) {
+            REQUIRE_THAT( x, IsFloat() );
+            CHECK( x.fval == Approx( 0.5 ) );
+        }
+    }
+
+    SECTION( "can be negative" ) {
+        for( const auto& x : sec.xs[ 3 ].xs.front() ) {
+            REQUIRE_THAT( x, IsFloat() );
+            CHECK( x.fval == Approx( -0.5 ) );
+        }
+    }
+
+    SECTION( "can be written in exponential notation" ) {
+        for( const auto& x : sec.xs[ 4 ].xs.front() ) {
+            REQUIRE_THAT( x, IsFloat() );
+            CHECK( x.fval == Approx( 50 ) );
+        }
+    }
+
+    SECTION( "can be negative with exponential notation" ) {
+        for( const auto& x : sec.xs[ 5 ].xs.front() ) {
+            REQUIRE_THAT( x, IsFloat() );
+            CHECK( x.fval == Approx( -50 ) );
+        }
+    }
+
+    SECTION( "can have negative exponent" ) {
+        for( const auto& x : sec.xs[ 6 ].xs.front() ) {
+            REQUIRE_THAT( x, IsFloat() );
+            CHECK( x.fval == Approx( 0.005 ) );
+        }
     }
 }
 
-BOOST_AUTO_TEST_CASE( boolean_string_yes ) {
-    const std::string inputs[] = {
-R"(
+TEST_CASE( "simple strings", "[string]") {
+    const std::string input = R"(
 RUNSPEC
+
 GRIDOPTS
-    1*'YES' 0 /
-)",
+    'YES' /
 
-R"(
-RUNSPEC
 GRIDOPTS
-    'Y' 0 /
-)",
+    YES /
 
-R"(
-RUNSPEC
 GRIDOPTS
-    1*YES 0 0 /
-)",
+    2*'YES' /
 
-R"(
-RUNSPEC
 GRIDOPTS
-    Y 0 /
-)",
+    2*YES /
 
-};
+GRIDOPTS
+    YES/
+)";
 
-    const std::string results[] = { "YES", "Y", "YES", "Y" };
+    auto sec = lun::parse( input.begin(), input.end() ).front();
+    REQUIRE( sec.name == "RUNSPEC" );
+    REQUIRE( sec.xs.size() == 5 );
 
-    auto isize = std::distance( std::begin( inputs ), std::end( inputs ) );
-    auto rsize = std::distance( std::begin( results ), std::end( results ) );
-    BOOST_CHECK_EQUAL( isize, rsize );
+    for( const auto& kw : sec.xs )
+        CHECK( kw.name == "GRIDOPTS" );
 
-    int i = 0;
-    for( const auto& input : inputs ) {
-        const auto& result = results[ i++ ];
+    for( const auto& kw : sec.xs )
+        REQUIRE( kw.xs.front().size() == 1 );
 
-        auto sec = parse( input.begin(), input.end() ).front();
-        const auto& kw = at( sec, "GRIDOPTS" ).at( 0 );
-
-        const auto& x1 = kw.at( 0 );
-        BOOST_CHECK_EQUAL( item::tag::str, x1.type );
-        BOOST_CHECK_EQUAL( x1.sval, result );
-
-        const auto& x2 = kw.at( 1 );
-        BOOST_CHECK_EQUAL( item::tag::i, x2.type );
-        BOOST_CHECK_EQUAL( x2.ival, 0 );
+    SECTION( "can be quoted" ) {
+        const auto& x = sec.xs[ 0 ].xs.front().front();
+        REQUIRE_THAT( x, IsString() );
+        CHECK( x.sval == "YES" );
     }
-}
 
+    SECTION( "can omit quotes" ) {
+        const auto& x = sec.xs[ 1 ].xs.front().front();
+        REQUIRE_THAT( x, IsString() );
+        CHECK( x.sval == "YES" );
+    }
+
+    SECTION( "can be repeated with quotes" ) {
+        const auto& x = sec.xs[ 2 ].xs.front().front();
+        REQUIRE_THAT( x, IsString() );
+        CHECK_THAT( x, Repeats( 2 ) );
+        CHECK( x.sval == "YES" );
+    }
+
+    SECTION( "can be repeated without quotes" ) {
+        const auto& x = sec.xs[ 3 ].xs.front().front();
+        REQUIRE_THAT( x, IsString() );
+        CHECK_THAT( x, Repeats( 2 ) );
+        CHECK( x.sval == "YES" );
+    }
+
+    SECTION( "stops at slash without whitespace" ) {
+        const auto& x = sec.xs[ 4 ].xs.front().front();
+        REQUIRE_THAT( x, IsString() );
+        CHECK( x.sval == "YES" );
+    }
 }
